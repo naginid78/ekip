7209f9f7ca1b90c1495ab01975299bae1fc57154
naginid78 <58333761+naginid78@users.noreply.github.com>
2020-12-06 03:31:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ def index():
 @app.route("/maison/<int:id>", methods=["GET"])
 def get_house_id(id):
 # permet d'obtenir une par id (on pourrait faire la meme chose par nom) et l'afficher dans un template
-
 	maison = None
 	for val in maisons:
 		if val.get("id") == id :
 			maison=val
 			break
 	return render_template("maison.html", maisons=maisons)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
